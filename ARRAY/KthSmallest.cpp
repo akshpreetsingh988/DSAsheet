ec30ee465aa28ad386a9497b854abf8c5b998a70
Aksh <56709048+akshpreetsingh988@users.noreply.github.com>
2021-07-11 12:57:24 +0530

    Add files via upload

---
+++ ARRAY/KthSmallest.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+    public:
+     int partition(int arr[], int l, int h){
+        int i=l-1;
+        int pi = l + (rand()%(h-l+1));
+        swap(arr[pi],arr[h]);
+        int piv = arr[h];
+        for(int j=l; j<h; j++){
+            if(arr[j]<piv){
+                 i++;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i+1], arr[h]);
+        return i+1;
+    }
+
+    int kthSmallest(int arr[], int l, int r, int k) {
+        while(l<=r){
+            int p = partition(arr, l, r);
+            if(p==k-1) return arr[p];
+            else if(p>k-1){
+               r = p-1;
+            } else {
+                l=p+1;
+            }
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
